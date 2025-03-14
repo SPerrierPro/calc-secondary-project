c3c64e2b60e8d44914b9a50f0405e7b0c30d433f
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-03-14 14:01:46 +0100

    start of the project

---
+++ index.js
@@ -0,0 +1,21 @@
+/*const onScreen = "";
+function writeOnScreen(e) {
+    console.log(e);
+    const input = document.getElementsByTagName("button");
+    console.log(input);
+    if (input !== "=") {
+        onScreen += input;
+
+    }
+}
+*/
+
+const inputs = document.getElementsByTagName("button");
+let onScreen = "";
+for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    input.onclick = function writeOnScreen() {
+        onScreen += input.value;
+        console.log(onScreen);
+    }
+}
