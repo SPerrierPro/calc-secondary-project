85fe1637cf9a41a37a453e2714f1927947ae93e9
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-03-14 15:29:57 +0100

    now screens only integers

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ let onScreen = "";
 for (let i = 0; i < inputs.length; i++) {
     const input = inputs[i];
     input.onclick = function writeOnScreen() {
-        onScreen += input.value;
-        console.log(onScreen);
-        const pScreen = document.querySelector("p");
-        pScreen.innerHTML = onScreen;
+        if (Number.isInteger(parseInt(input.value))) {
+            onScreen += input.value;
+            const pScreen = document.querySelector("p");
+            pScreen.innerHTML = onScreen;
+            let realNumber = parseInt(onScreen);
+        }
     }
+
+
 }
+
